5bf39ce9b2c7c7154294e8ef5562dfe2871aa55d
wilsonhybrid <wilsonhybrid@gmail.com>
2021-12-13 20:39:10 +0000

    fix some names

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const PREFIXES = ["cheese","cheesy","cheesey","cheg","cheggy","cheggie","chig","chiggy","chiggie","chug","chuggy","chuggie","smeg","smeggy","billy","bunty","boat","boatie","boaty","granny","crawly","crawlie","dandy","dandie","shandy","shandie","doodle","moody","randy","puddle","rolly","rollie","saw","sour","wood","woody","woodly","woodie"];
-const SUFFIXES = ["bug","bugger","dude","boy","dude","son","baker","postman","milkman","pig","pog","pug","piggy","piglet","hog","hoglet","louse","shrimp","bob","builder","button","daddy","papa","peg","grunter","slater","pea","menace","beetle","wig","weg","wug","saw","sour","wood","crawler","wibble"];
+const SUFFIXES = ["bug","bugger","dude","boy","dude","son","baker","postman","milkman","pig","pog","pug","piggy","piglet","hog","hoglet","louse","shrimp","bob","builder","button","daddy","papa","peg","grunter","slater","pea","menace","beetle","wig","weg","wug","wood","crawler","wibble","nibbler","polie","poly"];
 
 function generate() {
     document.getElementById("prefix").innerHTML = PREFIXES[Math.floor(Math.random() * PREFIXES.length)];
